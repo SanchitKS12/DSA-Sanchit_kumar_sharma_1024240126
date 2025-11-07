080b90834ab7160f748e70631cb80502314e8d19
SanchitKS12 <sanchit.sharma3412@gmail.com>
2025-11-07 15:02:05 +0530

    Question 2

---
+++ ASSINGMENT_8/ASSINGMENT_8_lab/Question_2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+Node* insert(Node* root, int key) {
+    if (root == nullptr)
+        return new Node(key);
+
+    if (key < root->data)
+        root->left = insert(root->left, key);
+    else if (key > root->data)
+        root->right = insert(root->right, key);
+
+    return root;
+}
+
+//Recursive Search(Search1)
+Node* Search1(Node* root, int key){
+    if(root == nullptr || root->data){
+        return root;
+    }
+    if(key > root->data){
+        return Search1(root->left, key);
+    }
+    if(key < root->data){
+         return Search1(root->right, key);
+    }
+}
+
+//Non Recursive(Search 2)
+Node* Search2(Node* root, int key){
+     while (root != nullptr) {
+        if (key == root->data)
+            return root;
+        else if (key < root->data)
+            root = root->left;
+        else
+            root = root->right;
+    }
+    return 0;
+}
+
+//
