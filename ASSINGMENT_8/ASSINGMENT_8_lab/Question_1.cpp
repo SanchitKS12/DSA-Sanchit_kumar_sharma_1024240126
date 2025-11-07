2a0d0e0840740291a92754f03117177ef74c5137
SanchitKS12 <sanchit.sharma3412@gmail.com>
2025-11-07 14:33:38 +0530

    Question_1

---
+++ ASSINGMENT_8/ASSINGMENT_8_lab/Question_1.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data=val;
+        left=right=nullptr;
+
+    }
+};
+
+void preorderTransversal(Node* root){
+    if(root == nullptr){
+        return;
+    }
+
+    cout<< root->data << " ";
+
+    preorderTransversal(root->left);
+    preorderTransversal(root->right);
+
+}
+
+void postorderTransversal(Node* root){
+    if(root == nullptr){
+        return;
+    }
+
+    postorderTransversal(root->left);
+    postorderTransversal(root->right);
+
+    cout<< root->data << " ";
+
+}
+
+void inorderTransversal(Node* root){
+    if(root == nullptr){
+        return;
+    }
+
+    inorderTransversal(root->left);
+
+    cout<< root->data << " ";
+
+    inorderTransversal(root->right);
+
+}
